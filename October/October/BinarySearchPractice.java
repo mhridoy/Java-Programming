811298ea124b9e9915234b25358860ec2edb1067
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-13 17:09:39 -0400

    practice

---
+++ October/October/BinarySearchPractice.java
@@ -0,0 +1,43 @@
+package October;
+
+import java.util.Scanner;
+
+public class BinarySearchPractice {
+
+    static int biSearch(int array[], int lengthArray, int target) {
+        int start = 0;
+        int end = lengthArray - 1;
+
+        while (start <= end) {
+            int mid_position = (start + end) / 2;
+
+            if (array[mid_position] == target)
+                return mid_position;
+
+            else if (array[mid_position] > target)
+                end = mid_position - 1;
+            else {
+                start = mid_position + 1;
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+
+        int array[]={10,20,30,40,50,60,70,80,90};
+        int target = in.nextInt();
+        int position = biSearch(array,array.length,target);
+
+        if(position==-1)
+            System.out.println("Not Found");
+        else {
+            System.out.println("Founf in :" + position);
+        }
+
+    }
+
+}
