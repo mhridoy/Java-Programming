fdf37a8f567e9c295801d4c5053de593e1ec16c5
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-13 21:33:49 -0400

    Update

--- October/October/Pattern_assignments/Pattern_02.java
+++ October/October/Pattern_assignments/Pattern_02.java
@@ -6,10 +6,11 @@ public class Pattern_02 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-         int row = in.nextInt();
-         int col=in.nextInt();
+        int row = in.nextInt();
+        int col = in.nextInt();
+
         for (int i = 1; i <= row; i++) {
-            for (int j = col-1; j >= i; j--) {
+            for (int j = col - 1; j >= i; j--) {
                 System.out.print(" ");
             }
             for (int k = 1; k <= i; k++) {
@@ -18,17 +19,15 @@ public class Pattern_02 {
             System.out.println();
         }
 
-
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
             }
-            for (int k = col-1; k >=i ; k--) {
+            for (int k = col - 1; k >= i; k--) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
     }
 }
 
