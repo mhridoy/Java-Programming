e8a45cde8594b56d9dff49ec1478ab05c8bf8be9
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-13 20:50:36 -0400

    Patterns

--- October/October/Pattern_assignments/Pattern_01.java
+++ October/October/Pattern_assignments/Pattern_01.java
@@ -1,7 +1,6 @@
 package October.Pattern_assignments;
 
 public class Pattern_01 {
-
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
