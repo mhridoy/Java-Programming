56df01d854802dccf03639ae695428876b7dc23f
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-13 21:32:54 -0400

    RightPattern

---
+++ October/October/Pattern_assignments/Pattern_02.java
@@ -0,0 +1,34 @@
+package October.Pattern_assignments;
+
+import java.util.Scanner;
+
+public class Pattern_02 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+         int row = in.nextInt();
+         int col=in.nextInt();
+        for (int i = 1; i <= row; i++) {
+            for (int j = col-1; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = col-1; k >=i ; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
