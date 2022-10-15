317cb5daa1d6b64d820456b44a7d71aba6a09833
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2022-10-15 07:02:13 +0600

    Update bafana_bafana.java

--- October/October/bafana_bafana.java
+++ October/October/bafana_bafana.java
@@ -10,21 +10,28 @@ public class bafana_bafana {
         int testCase = in.nextInt();
 
         for (int i = 1; i <=testCase; i++) {
-            int Players = in.nextInt();
-            int startPoint = in.nextInt();
-            int passess = in.nextInt();
+            int Players = in.nextInt(); // 5
+            int startPoint = in.nextInt(); //2 
+            int passess = in.nextInt(); //10
 
-            int total = startPoint + passess;
-            int playerstoBack = total - Players;
-            int result = playerstoBack;
+            int total = startPoint + passess; //12
+           int result = total%Players;
+            //int temp = Players;
+            // while(total>0){
+            //     total = total-Players; // 7 2 
+            //     if(total<=Players) break;
+            // }
+            if(result==0)
+            System.out.println("Case " + i+ ": " + Players);
+            else 
+            System.out.println("Case " + i+ ": " + Math.abs(result));
+            // if(total>Players  ){
 
-            if(total>Players  ){
+            //     System.out.println("case " + i+ ": " + result);}
 
-                System.out.println("case " + i+ ": " + result);}
+            // else if (total == Players)
 
-            else if (total == Players)
-
-                System.out.println("case " + i+ ": " + Players);
+            //     System.out.println("case " + i+ ": " + Players);
         }
     }
 }
