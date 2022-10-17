e488b0926148e21f4bdff3bddbb49fce4e7bc3e3
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-16 22:53:38 -0400

    update

--- October/October/bafana_bafana.java
+++ October/October/bafana_bafana.java
@@ -1,7 +1,6 @@
 package October;
 
 import java.util.Scanner;
-
 public class bafana_bafana {
 
     public static void main(String[] args) {
@@ -14,17 +13,14 @@ public class bafana_bafana {
             int startPoint = in.nextInt();
             int passess = in.nextInt();
 
-            int total = startPoint + passess;
-            int playerstoBack = total - Players;
-            int result = playerstoBack;
+            int total = startPoint + passess; 
+            int result = total%Players;
 
-            if(total>Players  ){
+            if(result==0)
+                System.out.println("Case " + i+ ": " + Players);
+            else
+                System.out.println("Case " + i+ ": " + Math.abs(result));
 
-                System.out.println("case " + i+ ": " + result);}
-
-            else if (total == Players)
-
-                System.out.println("case " + i+ ": " + Players);
         }
     }
 }
