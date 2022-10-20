306d6d91ae4e8006e0c59178501a62562018a9da
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-19 21:59:00 -0400

    upload

---
+++ October/October/Pattern_assignments/Pattern01.java
@@ -0,0 +1,17 @@
+package October.Pattern_assignments;
+
+public class Pattern01 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=4 ; i++) {
+
+            for (int j = 1; j <=4 ; j++) {
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
