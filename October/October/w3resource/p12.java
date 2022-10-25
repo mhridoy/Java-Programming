71a227e2cf82b77198e4216202fc0ab5d3a9d5a6
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-25 11:46:58 -0400

    Update p12.java

--- October/October/w3resource/p12.java
+++ October/October/w3resource/p12.java
@@ -3,9 +3,9 @@ package October.w3resource;
 public class p12 {
     public static void main(String[] args) {
 
-        for ( int i = 1; i <=4 ; i++) {
+        for ( int i = 1; i <=5 ; i++) {
             for (int j =1; j <=i ; j++) {
-                System.out.print(j );
+                System.out.print(j+" " );
             }
             System.out.println();
         }
