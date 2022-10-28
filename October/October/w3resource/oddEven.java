ab76326287c3da99a098e1253f259be02e57aafc
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-27 22:30:43 -0400

    update

---
+++ October/October/w3resource/oddEven.java
@@ -0,0 +1,18 @@
+package October.w3resource;
+
+public class oddEven {
+    public static void main(String[] args) {
+        for (int i = 1; i <=100 ; i++) {
+
+        if(i%2==0){
+
+            System.out.println(i +" : "+ "even");}
+
+            else
+            System.out.println(i +" : "+ "odd");
+
+        }
+        }
+
+    }
+
