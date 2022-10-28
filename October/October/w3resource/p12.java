ede29b6c61abf91b415f55d184fe3f3624a37f9d
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-27 22:32:14 -0400

    update

--- October/October/w3resource/p12.java
+++ October/October/w3resource/p12.java
@@ -3,11 +3,13 @@ package October.w3resource;
 public class p12 {
     public static void main(String[] args) {
 
-        for ( int i = 1; i <=5 ; i++) {
+        int count=1;
+        for ( int i = 1; i <=4 ; i++) {
             for (int j =1; j <=i ; j++) {
-                System.out.print(j+" " );
-            }
-            System.out.println();
-        }
+                System.out.print(count+" " );
+                count=count+2;
+
+        }System.out.println();
+
     }
-}
+}}
---
+++ October/October/w3resource/p14.java
@@ -0,0 +1,15 @@
+package October.w3resource;
+
+public class p14 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=4 ; i++) {
+            for (int j = 3; j >=i ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
