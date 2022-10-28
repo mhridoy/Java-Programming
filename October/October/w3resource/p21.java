0d9c5a291d6ff1f0dc2cc16fcc83d30a0c411031
shahnaz-43 <kirei.kanan@gmail.com>
2022-10-28 12:35:22 -0400

    Update p21.java

--- October/October/w3resource/p21.java
+++ October/October/w3resource/p21.java
@@ -3,13 +3,13 @@ package October.w3resource;
 public class p21 {
     public static void main(String[] args) {
 
-            int t=9;
+            int Se=9;
             int sum=0;
 
         for (int i = 1; i <= 5; i++) {
-                sum=sum+t;
-                System.out.print(t+" ");
-                t=10*t+9;
+                sum=sum+Se;
+                System.out.print(Se+" ");
+                Se=10*Se+9;
 
             }
         System.out.println();
