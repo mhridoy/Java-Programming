207d2e4dead48e5dc5e669ee0208f83c3d8b4494
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-03 23:34:48 -0400

    update

--- October/October/GivenRangePrimeSum.java
+++ October/October/GivenRangePrimeSum.java
@@ -1,11 +1,11 @@
 package October;
 
-public class GivenRangePrime {
+public class GivenRangePrimeSum {
     public static void main(String[] args) {
 
         int n1 = 2;
         int n2 = 50;
-
+        int sum = 0;
         for (int number = n1; number <= n2; number++) {
             int count = 0;
             for (int i = 2; i < number; i++) {
@@ -17,7 +17,11 @@ public class GivenRangePrime {
             }
             if (count == 0) {
                 System.out.print(number + " ");
+                sum = sum + number;
             }
+
         }
+        System.out.println();
+        System.out.println("Sum is " + sum);
     }
 }
