92882d790a5f4f14dde734373cf607efcdaee5f7
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-04 22:26:51 -0400

    update

--- October/October/LeapYear.java
+++ October/October/LeapYear.java
@@ -6,14 +6,19 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Please Enter a year , 1582 and above : ");
+        System.out.println("Please Enter a year, 1582 and above : ");
 
         int year = in.nextInt();
 
-        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
-            System.out.println(year + " is leap year.");
+        if (year >= 1582) {
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                System.out.println(year + " is leap year.");
 
+            else
+                System.out.println(year + " is not leap year.");
+        }
         else
-            System.out.println(year + " is not leap year.");
+            System.out.println("Please Enter a year, 1582 and above!");
     }
+
 }
