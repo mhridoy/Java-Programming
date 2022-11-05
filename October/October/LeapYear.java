cda03f812e49b6078edc268c14c16e225402f563
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-04 21:57:36 -0400

    update

--- October/October/LeapYear.java
+++ October/October/LeapYear.java
@@ -5,10 +5,7 @@ public class LeapYear {
 
         int year = 1700;
 
-        if(year%400 ==0)
-            System.out.println(year + " is leap year.");
-
-        if (year%4==0 && year%100!=0)
+        if((year%400 ==0)|| (year%4==0 && year%100!=0))
             System.out.println(year + " is leap year.");
 
         else
