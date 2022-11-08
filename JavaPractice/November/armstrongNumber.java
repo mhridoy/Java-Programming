6dd2ba7fc91184800afa4458988e7ed618b4a931
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-07 21:03:39 -0500

    update

---
+++ JavaPractice/November/armstrongNumber.java
@@ -0,0 +1,25 @@
+package November;
+
+import java.util.Scanner;
+
+public class armstrongNumber {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int temp = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int vf1 = num / 10;
+            int rem = num % 10;
+            num = vf1;
+            sum = sum + (rem * rem * rem);
+        }
+
+        if (temp == sum)
+            System.out.println(temp + " is armstrong number.");
+        else
+            System.out.println(temp + " is not armstrong number.");
+    }
+}
