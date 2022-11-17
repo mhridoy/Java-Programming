5cef5b387a469c8f853085992ef692532722551d
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-16 22:35:03 -0500

    upload

---
+++ JavaPractice/November/StringPractice02.java
@@ -0,0 +1,14 @@
+package November;
+
+public class Stringp02 {
+    public static void main(String[] args) {
+        String name= "shahnaz";
+
+      for (int i = 0; i <name.length() ; i++) {
+            int poition = name.charAt(i)-32;
+            System.out.print((char) poition);
+        }
+
+
+    }
+}
