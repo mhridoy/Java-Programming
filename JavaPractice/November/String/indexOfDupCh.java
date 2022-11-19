933666958916fa47e2a312241646e09548dfd286
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-18 20:58:29 -0500

    upload

---
+++ JavaPractice/November/String/indexOfDupCh.java
@@ -0,0 +1,17 @@
+package November.String;
+
+public class indexOfDupCh {
+    public static void main(String[] args) {
+
+        String name = "shahnaz aisha";
+        for (int i = 0; i <name.length(); i++) {
+
+            if(name.charAt(i)=='h')
+                System.out.println(i);
+         //   System.out.println(name.charAt(i));
+
+
+        }
+    }}
+
+
