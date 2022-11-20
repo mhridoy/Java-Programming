351a1514f92810fd91857951aa0431240058ee95
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-20 01:40:14 -0500

    upload

---
+++ JavaPractice/November/String/practice/ascii.java
@@ -0,0 +1,11 @@
+package November.String.practice;
+
+public class ascii {
+    public static void main(String[] args) {
+        for (int i = 0; i <1 ; i++) {
+
+            System.out.println(i+'_');
+
+        }
+    }
+}
