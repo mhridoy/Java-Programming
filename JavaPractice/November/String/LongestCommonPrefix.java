8e4f8e0468327bd177b57c4d6b3a44d1322e3f81
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-23 23:59:42 -0500

    update

--- JavaPractice/November/String/LongestCommonPrefix.java
+++ JavaPractice/November/String/LongestCommonPrefix.java
@@ -5,25 +5,30 @@ import java.util.Arrays;
 
 public class LongestCommonPrefix
 {
+
+    public static void main(String[] args)
+    {
+        LongestCommonPrefix gfg = new LongestCommonPrefix();
+        String[] input = {"geeksforgeeks", "geeks", "geek", "geezer"};
+        System.out.println( "The longest Common Prefix is : " +
+                gfg.longestCommonPrefix(input));
+    }
     public String longestCommonPrefix(String[] a)
     {
         int size = a.length;
 
-        /* if size is 0, return empty string */
         if (size == 0)
             return "";
 
         if (size == 1)
             return a[0];
-
-        /* sort the array of strings */
+        
         Arrays.sort(a);
 
-        /* find the minimum length from first and last string */
+
         int end = Math.min(a[0].length(), a[size-1].length());
 
-        /* find the common prefix between the first and
-           last string */
+
         int i = 0;
         while (i < end && a[0].charAt(i) == a[size-1].charAt(i) )
             i++;
@@ -32,12 +37,5 @@ public class LongestCommonPrefix
         return pre;
     }
 
-    /* Driver Function to test other function */
-    public static void main(String[] args)
-    {
-        LongestCommonPrefix gfg = new LongestCommonPrefix();
-        String[] input = {"geeksforgeeks", "geeks", "geek", "geezer"};
-        System.out.println( "The longest Common Prefix is : " +
-                gfg.longestCommonPrefix(input));
-    }
+
 }
