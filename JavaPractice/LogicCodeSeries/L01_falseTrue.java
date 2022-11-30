2adb31b356f117bc7e25be2f3ccfd87e25644f52
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-29 21:11:31 -0500

    Upload

---
+++ JavaPractice/LogicCodeSeries/L01_falseTrue.java
@@ -0,0 +1,12 @@
+package LogicCodeSeries;
+
+public class L01_falseTrue {
+    public static void main(String[] args) {
+        String og="java";
+        String og2= new String(og);
+
+        System.out.println((og==og2)+" "+ (og.equals(og2)));
+
+    }
+}
+
