10d3fd82365cdbdb43a17fb0739dc592d7ecbb91
shahnaz-43 <kirei.kanan@gmail.com>
2022-11-30 14:21:21 -0500

    Update

--- JavaPractice/CodeForces/Atillas_Favorite_Problem.java
+++ JavaPractice/CodeForces/Atillas_Favorite_Problem.java
@@ -1,10 +1,10 @@
-//B. Atilla's Favorite Problem
-package November;
+//Codeforces Round #835 (Div. 4)
+package CodeForces;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class CodeforcesRound_835_Div4 {
+public class Atillas_Favorite_Problem {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
@@ -13,9 +13,8 @@ public class CodeforcesRound_835_Div4 {
             int alphabetSize = in.nextInt();
                 String s = in.next();
                 char ch[] = s.toCharArray();
-                Arrays.sort(ch); 
+                Arrays.sort(ch);
                 int position = (int) ch[ch.length - 1];
-
                 System.out.println(position - 96);
 
             }
