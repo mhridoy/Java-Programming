92de1bf5a431b5e7a878267039f6715fdc74f035
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-01 14:17:37 -0500

    upload

---
+++ JavaPractice/LogicCodeSeries/L02_Range.java
@@ -0,0 +1,12 @@
+package LogicCodeSeries;
+
+public class L02_Range {
+    public static void main(String[] args) {
+
+        Integer a=128,b=128;
+        Integer c=100,d=100;
+
+        System.out.println(a==b);//false
+        System.out.println(c==d);//true
+    }
+}
