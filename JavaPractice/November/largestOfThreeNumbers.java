ef6e77470b706ced252ec46cef3e0bf6c06fb7ab
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-03 23:42:46 -0500

    Upload

---
+++ JavaPractice/November/largestOfThreeNumbers.java
@@ -0,0 +1,27 @@
+package November;
+
+import java.util.Scanner;
+
+public class largestOfThreeNumbers {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int numOne= in.nextInt();
+        int numTwo= in.nextInt();
+        int numThree= in.nextInt();
+
+
+        if(numOne>numTwo && numOne>numThree){
+            System.out.println("Largest number is = " + numOne);
+        }
+        if(numTwo>numOne && numTwo>numThree){
+            System.out.println("Largest number is = " + numTwo);
+
+    }
+
+            if(numThree>numOne && numThree>numTwo){
+            System.out.println("Largest number is = " + numThree);
+        }
+    }
+}
