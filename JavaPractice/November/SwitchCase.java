5bcc9fc41a60fc088ef88640015d5316b9b6a1a0
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-04 20:12:01 -0500

    Update SwitchCase.java

--- JavaPractice/November/SwitchCase.java
+++ JavaPractice/November/SwitchCase.java
@@ -2,8 +2,35 @@ package November;
 
 public class SwitchCase {
     public static void main(String[] args) {
-        int n = 3;
-        switch (n) {
+        String str = "Ctg";
+        switch (str) {
+            case "Dhaka":
+                System.out.println("Dhaka is beautiful");
+                break;
+            case "Ctg":
+                System.out.println("Chittagong is amazing");
+                break;
+            default:
+                System.out.println("Pleae Enter right word");
+
+        }
+       /* char ch='c';
+        switch (ch){
+            case 'A':
+                System.out.println("A");
+            break;
+            case 'B':
+                System.out.println("B");
+                break;
+            default:
+                System.out.println("No ch Found");
+        }*/
+//        double d=5.5;
+//        switch ((int) d){
+//        }
+
+        // int n = 6;
+     /*  switch (n) {
             case 1:
                 System.out.println("One");
                 break;
@@ -19,6 +46,9 @@ public class SwitchCase {
             case 5:
                 System.out.println("Five");
                 break;
-        }
+            default:
+                System.out.println("Not found");*/
+
     }
 }
+
