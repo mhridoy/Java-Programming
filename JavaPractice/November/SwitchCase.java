c02a4a98d480827b254c66d595fafc287a9b6441
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-04 19:52:24 -0500

    Upload

---
+++ JavaPractice/November/SwitchCase.java
@@ -0,0 +1,19 @@
+package November;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        int n = 5;
+        switch (n) {
+            case 1:
+                System.out.println("One");
+            case 2:
+                System.out.println("Two");
+            case 3:
+                System.out.println("Three");
+            case 4:
+                System.out.println("Four");
+            case 5:
+                System.out.println("Five");
+        }
+    }
+}
