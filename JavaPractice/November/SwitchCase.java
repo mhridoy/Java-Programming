ed09b251bce79d4211916a6172ffeb97074f7e49
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-04 19:54:21 -0500

    Update SwitchCase.java

--- JavaPractice/November/SwitchCase.java
+++ JavaPractice/November/SwitchCase.java
@@ -2,18 +2,23 @@ package November;
 
 public class SwitchCase {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 3;
         switch (n) {
             case 1:
                 System.out.println("One");
+                break;
             case 2:
                 System.out.println("Two");
+                break;
             case 3:
                 System.out.println("Three");
+                break;
             case 4:
                 System.out.println("Four");
+                break;
             case 5:
                 System.out.println("Five");
+                break;
         }
     }
 }
