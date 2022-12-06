a89abfc2ecf422c398e34bf9714c0ad2c769ff14
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-05 22:00:38 -0500

    update

--- JavaPractice/November/String/StrEqual.java
+++ JavaPractice/November/String/StrEqual.java
@@ -4,9 +4,9 @@ public class StrEqual {
 
     public static void main(String[] args) {
         String str1 = "hello";
-        String str2 = new String("hello");
+        String str2 = new String(str1);
         String str3 = "hello";
-        if (str1 == str2) {
+        if (str1 == str2) { // if (str1.equals(str2))
             System.out.println("str1 and str2 equal");
         } else {
             System.out.println("str1 and str2 not equal");
