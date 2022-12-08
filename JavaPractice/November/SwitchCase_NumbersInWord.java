91be9f0aaa3cd284d1940a88ce9214fdf60f0f35
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-08 12:17:46 -0500

    Update SwitchCase_NumbersInWord.java

--- JavaPractice/November/SwitchCase_NumbersInWord.java
+++ JavaPractice/November/SwitchCase_NumbersInWord.java
@@ -8,13 +8,15 @@ public class SwitchCase_NumbersInWord {
         System.out.println("Enter a number");
         int n = scn.nextInt();
         String str = "";
+        String str2 = "";
         while (n > 0) {
-            int r = n % 10;
+            int rem = n % 10;
             // System.out.println(r);
             n = n / 10;
-            str = r + str;
+            str =rem+str;
+
         }
-        // System.out.println(str);
+         System.out.println(str);
 
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
