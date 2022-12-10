de55b9f1619fab57030f02d18943c63756e2203d
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-10 00:06:21 -0500

    Upload

---
+++ JavaPractice/November/String/RevStringBufferClass.java
@@ -0,0 +1,10 @@
+package November.String;
+
+public class RevStringBufferClass {
+    public static void main(String[] args) {
+
+        StringBuffer buffer=new StringBuffer("Java");
+
+        System.out.println(buffer.reverse());
+    }
+}
