82f61139145db03f097915c7b78b27c4508f7523
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-11 23:55:33 -0500

    upload

---
+++ JavaPractice/November/String/RemoveGivenCharacterFromString.java
@@ -0,0 +1,30 @@
+package November.String;
+
+public class RemoveGivenCharacterFromString {
+    public static void main(String[] args) {
+        String word = "Java";
+        //replacing character in this String
+        String replaced = word.replace("J", "K");
+        System.out.println("Replacing character in String");
+        System.out.println("Original String before replace : " + word);
+        System.out.println("Replaced String : " + replaced);
+
+        //replacing substring on String in Java
+        String str = "Scala is good programming language";
+        replaced = str.replaceAll("Scala", "Java");
+        System.out.println("String before replace : " + str);
+        System.out.println("String after replace : " + replaced);
+
+        //replacing all space in String with # using regular expression
+        replaced = str.replaceFirst("\\s", "#");
+        System.out.println("Replacing first match of regex using replaceFirst()");
+        System.out.println("Original String before replacement : " + str);
+        System.out.println("Final String : " + replaced);
+
+        System.out.println("Replacing all occurrence of substring which match regex");
+        replaced = str.replaceAll("\\s", "#");
+        System.out.println("ReplaceAll Example : " + replaced);
+    }
+
+}
+
