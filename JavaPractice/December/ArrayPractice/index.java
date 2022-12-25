0ac4097e1115f0f64ee8c8beb635d10e12101b4e
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-24 23:37:50 -0500

    upload

---
+++ JavaPractice/December/ArrayPractice/index.java
@@ -0,0 +1,9 @@
+package December.ArrayPractice;
+public class index {
+    public static void main(String[] args) {
+        int index = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(index++);
+        }
+    }
+}
