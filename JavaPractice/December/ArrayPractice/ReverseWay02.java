34953c27a7b0b3939ae3898d26cbfe641a19f4e9
shahnaz-43 <kirei.kanan@gmail.com>
2022-12-26 01:26:09 -0500

    upload

--- JavaPractice/December/ArrayPractice/ReverseWay02.java
+++ JavaPractice/December/ArrayPractice/ReverseWay02.java
@@ -5,19 +5,30 @@ public class ReverseWay02 {
 
 
     int arr[]={1,2,3,4,5};
+    String str[] = {"D","h","a","k","a"};
     int index=1;
-    int temp;
+    String temp;
 
-        for (int i = 0; i < arr.length/2 ; i++) {
-
-        temp=arr[i];
-        arr[i]=arr[arr.length-index];
-        arr[arr.length-index]=temp;
-        index++;
-
-    }
-        for (int i = 0; i < arr.length ; i++) {
-        System.out.print(arr[i]+" ");
-    }
+        for (int i = 0; i < str.length/2 ; i++) {
+            temp=str[i];
+            str[i]=str[str.length-index];
+            str[str.length-index]=temp;
+            index++;
+        }
+        for (int i = 0; i < str.length ; i++) {
+            System.out.print(str[i]+" ");
+        }
+//
+//        for (int i = 0; i < arr.length/2 ; i++) {
+//
+//        temp=arr[i];
+//        arr[i]=arr[arr.length-index];
+//        arr[arr.length-index]=temp;
+//        index++;
+//
+//    }
+//        for (int i = 0; i < arr.length ; i++) {
+//        System.out.print(arr[i]+" ");
+//    }
 
 }}
