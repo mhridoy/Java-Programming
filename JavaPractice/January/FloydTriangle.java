c4d0f82af678c4126de50aa17e2bb9a4cde22e23
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-04 11:44:57 -0500

    upload

---
+++ JavaPractice/January/FloydTriangle.java
@@ -0,0 +1,16 @@
+package January;
+
+public class FloydTriangle {
+
+    public static void main(String[] args) {
+        int row = 5;
+        int number=1;
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
