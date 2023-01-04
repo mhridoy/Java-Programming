94153cfce86e42acc9e8264e5e97adb8d300e596
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-04 11:42:36 -0500

    upload

---
+++ JavaPractice/January/Half_pyramid180.java
@@ -0,0 +1,28 @@
+package January;
+    public class Half_pyramid180 {
+        public static void main(String[] args) {
+            int n = 4;
+            for(int i=1; i<=n; i++) {
+                //First pyramid
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            //reverse pyramid
+            for(int i=n-1; i>=1; i--) {
+                //First pyramid
+                for (int j =n-1; j>=i; j--) {
+                    System.out.print(" ");
+                }
+                for (int j = i; j >= 1; j--) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
