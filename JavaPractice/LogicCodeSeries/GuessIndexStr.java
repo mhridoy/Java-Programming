12d24164f901cd9ad88a5c8ebbb63d2b8d1d9372
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-05 23:26:27 -0500

    Upload

---
+++ JavaPractice/LogicCodeSeries/GuessIndexStr.java
@@ -0,0 +1,8 @@
+package LogicCodeSeries;
+
+public class GuessIndexStr {
+    public static void main(String[] args) {
+        String str= "Hello";
+        System.out.println(str.indexOf('t'));
+    }
+}
