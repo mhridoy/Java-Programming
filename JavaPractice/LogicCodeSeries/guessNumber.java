8ed96de5bc06ae4061a574c46f8752d0585945a0
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-06 00:17:08 -0500

    Upload

---
+++ JavaPractice/LogicCodeSeries/guessNumber.java
@@ -0,0 +1,12 @@
+package LogicCodeSeries;
+
+public class guessNumber {
+    public static void main(String[] args) {
+        int[] array = {120, 200, 016};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+}
+//Answer: 120 200 14
+//016 is an octal number, its equivalent decimal number is 14
