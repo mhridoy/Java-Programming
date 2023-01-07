0e94db443481fb8c14e5c9f515af58ac6c970443
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-06 23:52:38 -0500

    Upload

---
+++ JavaPractice/January/StringContainsAlphabetsRegx.java
@@ -0,0 +1,15 @@
+package January;
+
+public class StringContainsAlphabetsRegx {
+    public static boolean isStringContainsAlpabets(String str) {
+        return ((str !=null)
+                && (!str.equals("")
+                && (str.matches("^[a-zA-Z]*$"))));
+    }
+
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        System.out.println("Input: " + str);
+        System.out.println("Output: "+ isStringContainsAlpabets(str));
+    }
+}
