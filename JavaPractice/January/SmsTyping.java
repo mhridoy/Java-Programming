acf0372c5782fecf8d32a016546fe3273e576e44
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-10 19:37:42 -0500

    Update

--- JavaPractice/January/SmsTyping.java
+++ JavaPractice/January/SmsTyping.java
@@ -12,17 +12,18 @@ public class SmsTyping {
         char ch6[]={'t','u','v'};
         char ch7[]={'w', 'x', 'y', 'z'};
         char ch8[]={' '};
-
         int count=0;
-        String str = "welcome to ulab";
+        String str = "good luck and have fun";//1 2 3 3 3 1 2 1 3 19
         for (int i = 0; i <str.length(); i++) {
             for (int j = 0; j < ch.length ; j++) {
                 if(str.charAt(i)==ch[j]   ||str.charAt(i)==ch1[j] ||str.charAt(i)==ch2[j] ||
                         str.charAt(i)==ch3[j] ||str.charAt(i)==ch4[j]||str.charAt(i)==ch5[j]||
-                        str.charAt(i)==ch6[j]||str.charAt(i)==ch7[j]||
-                        str.charAt(i)==ch8[0]  ){
+                        str.charAt(i)==ch6[j]||str.charAt(i)==ch7[j] ){
                     count=count+(j+1);
                 }
+            }
+            if(str.charAt(i)==ch8[0] ){
+                count=count+1;
             }
         }
         System.out.println(count);
