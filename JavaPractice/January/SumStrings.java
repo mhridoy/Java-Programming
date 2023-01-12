2f1c034c0330a8803cd756ad2c5bc8873181a384
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-12 18:55:28 -0500

    Upload

---
+++ JavaPractice/January/SumStrings.java
@@ -0,0 +1,13 @@
+package January;
+
+public class SumStrings {
+    public static void main(String[] args) {
+        String s1="123";
+        String s2="987";
+
+        int sum= Integer.parseInt(s1)+Integer.parseInt(s2);
+
+        System.out.println(sum);
+        System.out.println(String.valueOf(sum));
+    }
+}
