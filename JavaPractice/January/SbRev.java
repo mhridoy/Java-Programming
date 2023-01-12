b0c75d706fcea405bbdaab044cc8f4757c71d04a
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-12 13:05:46 -0500

    Update SbRev.java

--- JavaPractice/January/SbRev.java
+++ JavaPractice/January/SbRev.java
@@ -4,10 +4,13 @@ public class SbRev {
     public static void main(String[] args) {
         int num = 69875;
 
-        StringBuilder sb = new StringBuilder();
+        /*StringBuilder sb = new StringBuilder();
         sb.append(num);
-
         StringBuilder rev= sb.reverse();
+        System.out.println(rev);*/
+
+        StringBuffer sb = new StringBuffer(String.valueOf(num));
+        StringBuffer rev = sb.reverse();
         System.out.println(rev);
 
 
