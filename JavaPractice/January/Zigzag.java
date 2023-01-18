b907753659c0bb874850e5089b627cec46d2544f
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-17 20:55:23 -0500

    Upload

---
+++ JavaPractice/January/Zigzag.java
@@ -0,0 +1,14 @@
+package January;
+
+public class Zigzag {
+    public static void main(String[] args) {
+        int j=10;
+
+        for (int i = 1; i <=5 ; i++) {
+            System.out.println(i);
+            System.out.println(j);
+            j--;
+
+        }
+    }
+}
