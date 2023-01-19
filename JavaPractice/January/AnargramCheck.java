ee4a784e064aedfa38f993b3e036ff068ee11b46
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-18 23:24:45 -0500

    Create AnargramCheck.java
    upload

---
+++ JavaPractice/January/AnargramCheck.java
@@ -0,0 +1,30 @@
+package January;
+
+import java.util.HashSet;
+
+public class AnargramCheck {
+
+    public static void main(String args[]) {
+        String first = "mother";
+        String second = "father";
+        boolean result = isAnagram(first.toLowerCase().trim() , second.toLowerCase().trim());
+        System.out.println("Is Anagram = "+result);
+    }
+
+    public static boolean isAnagram(String first, String second) {
+
+        if(first.length() != second.length())
+            return false;
+
+        HashSet hs = new HashSet();
+        for(char ch : first.toCharArray()) {
+            hs.add(ch);
+        }
+        for(char ch : second.toCharArray()) {
+            /* if char is allowed in hashset then it means two strings do not contains same chars */
+            if(hs.add(ch))
+                return false;
+        }
+        return true;
+    }
+}
