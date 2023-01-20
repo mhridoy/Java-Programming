26a65ab0ba09857713889be9c8d41b81080a47fd
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-20 17:54:08 -0500

    Upload

---
+++ JavaPractice/January/BitWise/shift.java
@@ -0,0 +1,12 @@
+package January.BitWise;
+
+public class shift {
+    public static void main(String[] args) {
+        int w=5;
+        int x=2;
+        int y= w<<2;
+        int z= w>>2;
+        System.out.println("Left shift of 5 : " +y);
+        System.out.println("Right shift of 5 : " + z);
+    }
+}
