2ab23f2bf56255a73ab472a871deef799d33d114
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-21 21:06:41 -0500

    Update

--- JavaPractice/January/CopyAndReverseString.java
+++ JavaPractice/January/CopyAndReverseString.java
@@ -5,18 +5,18 @@ public class CopyAndReverseString {
         String org_str="Shahnaz";
         char ch[]= org_str.toCharArray();
         int size= ch.length;
-        char Copy1[]=new char[size];
+        char Copy[]=new char[size];
         char Reverse[]=new char[size];
         int i=0;
         while (i!=size){
-            Copy1[i] =ch[i];
+            Copy[i] =ch[i];
             Reverse[size-1-i]=ch[i];
             i++;
         }
         System.out.println("Original String  >> " + org_str);
         System.out.print("Copied String >> ");
-        System.out.println(Copy1);
-        
+        System.out.println(Copy);
+
         System.out.print("Reversed String >> ");
         System.out.println(Reverse);
     }
