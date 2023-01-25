afce415f1246602e9685aa80e20725bfa590be29
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-25 11:32:16 -0500

    Update PerfectNumber.java

--- JavaPractice/January/PerfectNumber.java
+++ JavaPractice/January/PerfectNumber.java
@@ -4,23 +4,18 @@ import java.util.Scanner;
 
 public class PerfectNumber {
     public static void main(String[] args) {
-
         // Scanner input = new Scanner(System.in);
-
-        int num = 6;
-
+        int num = 27;
         int sum = 0;
-
         for (int i = 1; i < num; i++) {
             if (num % i == 0) {
                 sum = sum + i;
             }
         }
-
         if (sum == num) {
-            System.out.println("Perfect");
+            System.out.println("It's a Perfect Number");
         } else {
-            System.out.println("Not Perfect");
+            System.out.println("It's not a Perfect Number");
         }
     }
 }
