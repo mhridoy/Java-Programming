5faf5fd165bcffd08fd33cc0858e764657c39dc7
shahnaz-43 <kirei.kanan@gmail.com>
2023-02-12 23:57:30 -0500

    Upload

---
+++ JavaPractice/February/CompoundInterest.java
@@ -0,0 +1,13 @@
+package February;
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+        double principle=10000;
+        double rate=0.05;
+        int time=5;
+        double interest = principle * Math.pow(1 +rate, time) - principle;
+        System.out.println("Interest is: " +
+                interest);
+
+    }
+}
