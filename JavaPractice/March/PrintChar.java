d291158a9b066ec4f313820d430452ff6d6f786b
shahnaz-43 <kirei.kanan@gmail.com>
2023-03-04 23:29:04 -0500

    Commit

---
+++ JavaPractice/March/PrintChar.java
@@ -0,0 +1,10 @@
+package March;
+
+public class PrintChar {
+    public static void main(String[] args) {
+        for (char c = 'A';  c <='Z' ; c++) {
+            System.out.print(c + " ");
+
+        }
+    }
+}
