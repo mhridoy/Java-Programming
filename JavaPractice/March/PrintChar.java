e6577a299a755de0c179a6fac6a23e322793fc13
shahnaz-43 <kirei.kanan@gmail.com>
2023-03-05 00:41:33 -0500

    Update

--- JavaPractice/March/PrintChar.java
+++ JavaPractice/March/PrintChar.java
@@ -2,8 +2,13 @@ package March;
 
 public class PrintChar {
     public static void main(String[] args) {
-        for (char c = 'A';  c <='Z' ; c++) {
-            System.out.print(c + " ");
+//        for (char c = 'A' ;  c <='Z' ; c++) {
+//            System.out.print(c + " ");
+//
+//        }
+
+        for (int i = 97; i <=122 ; i++) {
+            System.out.print((char)i + " ");
 
         }
     }
