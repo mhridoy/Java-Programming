01f2ebdcb7045aff32eaddce224bd0384863a1a6
shahnaz-43 <kirei.kanan@gmail.com>
2023-03-12 23:44:40 -0400

    Commit

---
+++ JavaPractice/March/CalculatePowerOfNum.java
@@ -0,0 +1,12 @@
+package March;
+
+public class CalculatePowerOfNum {
+    public static void main(String[] args) {
+        int base=-3;
+        int exponent=2;
+        double result = Math.pow(base,exponent);
+
+        System.out.println(result);
+
+    }
+}
