5e2d8004abc927e833ca9ac6866b9a3f7b684f71
shahnaz-43 <kirei.kanan@gmail.com>
2023-03-18 23:54:49 -0400

    Commit

--- JavaPractice/March/CollectionsDemo/HashMapDemo.java
+++ JavaPractice/March/CollectionsDemo/HashMapDemo.java
@@ -10,6 +10,8 @@ public class HashMapDemo {
         m.put(101,"Bread");
         m.put(102,"Banana");
         m.put(103,"Oat");
+        m.put(104,"Orange");
+        m.put(102,"Apple");
 
         System.out.println(m);
     }
